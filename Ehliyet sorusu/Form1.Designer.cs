823b337228bd29a08c537d606783dd2e966bbc75
ilayda yalçın <146716704+ilaydayacin@users.noreply.github.com>
2023-10-03 15:23:24 +0300

    Add files via upload

---
+++ Ehliyet sorusu/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Ehliyet_sorusu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtYas = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.btnEhliyet = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtYas
+            // 
+            this.txtYas.Location = new System.Drawing.Point(108, 31);
+            this.txtYas.Name = "txtYas";
+            this.txtYas.Size = new System.Drawing.Size(160, 20);
+            this.txtYas.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(21, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Yaş giriniz";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(21, 83);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(191, 20);
+            this.lblSonuc.TabIndex = 2;
+            this.lblSonuc.Text = "Ehliyet almaya uygun mu: ";
+            // 
+            // btnEhliyet
+            // 
+            this.btnEhliyet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEhliyet.Location = new System.Drawing.Point(103, 131);
+            this.btnEhliyet.Name = "btnEhliyet";
+            this.btnEhliyet.Size = new System.Drawing.Size(130, 78);
+            this.btnEhliyet.TabIndex = 4;
+            this.btnEhliyet.Text = "Alır mı \r\nAlamaz mı\r\n";
+            this.btnEhliyet.UseVisualStyleBackColor = true;
+            this.btnEhliyet.Click += new System.EventHandler(this.btnEhliyet_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(373, 249);
+            this.Controls.Add(this.btnEhliyet);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtYas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtYas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Button btnEhliyet;
+    }
+}
+
---
+++ Ehliyet sorusu/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ehliyet_sorusu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEhliyet_Click(object sender, EventArgs e)
+        {
+            int yas;
+            yas = Convert.ToInt32(txtYas.Text);
+
+            if (yas >= 18)
+            {
+                lblSonuc.Text = "Ehliyet almaya uygun mu: Uygun";
+            }
+            if (yas < 18)
+            {
+                lblSonuc.Text = "Ehliyet almaya uygun mu: Uygun değil";
+            }
+        }
+    }
+}
